86f7bab1d392e774b23b263cd389d3e4355adb11
DHN Chandan <php.chandan@gmail.com>
2023-02-22 06:01:30 +0600

    refactor: Update logics

--- main.py
+++ main.py
@@ -1,105 +1,70 @@
 import flet_core.icons
 from flet import *
-import csv
-from datetime import datetime
 import os
+import pandas as pd
+import hashlib
 
 
 def main(page: Page):
     page.padding = 20
-    page.title = "Jellop CSV Tool"
+    page.title = "Jellop CSV Hash Tool"
 
-    csv1_location = Text("")
-    csv2_location = Text("")
+    csv_location = Text("")
     result = Text("")
 
     def picker1_dialogue(e: FilePickerResultEvent):
-        print("CSV 1")
-        print(e.files)
         if e.files and len(e.files):
-            csv1_location.value = e.files[0].path
-            csv1_location.update()
-
-    def picker2_dialogue(e: FilePickerResultEvent):
-        print("CSV 2")
-        print(e.files)
-        if e.files and len(e.files):
-            csv2_location.value = e.files[0].path
-            csv2_location.update()
+            csv_location.value = e.files[0].path
+            csv_location.update()
 
     my_picker1 = FilePicker(on_result=picker1_dialogue)
     page.overlay.append(my_picker1)
     page.update()
 
-    my_picker2 = FilePicker(on_result=picker2_dialogue)
-    page.overlay.append(my_picker2)
-    page.update()
-
-    def process_search(e):
+    def process_hashing(e):
         result.value = ""
         result.update()
 
-        if csv1_location.value == "" or csv2_location.value == "":
-            result.value = "Please upload 2 csv files."
+        if csv_location.value == "":
+            result.value = "Please upload csv file."
             result.update()
             return
 
-        list2 = []
-        with open(csv2_location.value, encoding="ISO-8859-1") as f:
-            for idx, row in enumerate(f):
-                if idx != 0:
-                    list2.append(row.split()[0])
+        df = pd.read_csv(csv_location.value, encoding="ISO-8859-1")
+        first_header = list(df.columns)[0]
 
-        with open(csv1_location.value, encoding="ISO-8859-1") as f:
-            csv_file = csv.reader(f, delimiter=",")
-            headers = next(csv_file)
-            rows = []
+        # hashing the 'Password' column
+        df[first_header] = df[first_header].apply(lambda x: hashlib.sha256(str(x).encode('utf-8')).hexdigest())
 
+        dir_name = os.path.dirname(csv_location.value)
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
 
-            one_d_dict = {}
-            for value in list2:
-                one_d_dict[value] = True
+        _, tail = os.path.split(csv_location.value)
+        file_name, file_extension = os.path.splitext(tail)
 
-            for row in csv_file:
-                if row[0] in one_d_dict:
-                    rows.append(row)
+        save_file = f"{dir_name}/{file_name}_hashed{file_extension}"
 
-            file_location = os.path.dirname(csv2_location.value)
-            dir_name = os.path.join(file_location, "FB_upload")
-            if not os.path.exists(dir_name):
-                os.makedirs(dir_name)
+        # writing the new CSV output
+        df.to_csv(save_file, index=False)
 
-            _, tail = os.path.split(csv2_location.value)
+        result.value = f"Hash completed. File saved to {save_file}"
+        result.update()
 
-            save_file = f"{dir_name}/FB_upload_{tail}"
-
-            with open(save_file, 'w', newline='', encoding='utf-8') as c:
-                write = csv.writer(c)
-                write.writerow(headers)
-                write.writerows(rows)
-
-            result.value = f'File saved in {save_file}'
-            result.update()
-            return
+        return
 
     page.add(
         Column([
             Column([
                 ElevatedButton(
-                    "Pick CSV 1",
+                    "Pick CSV",
                     icon=flet_core.icons.UPLOAD_FILE,
                     on_click=lambda _: my_picker1.pick_files(allow_multiple=False)
                 ),
-                csv1_location,
+                csv_location,
                 ElevatedButton(
-                    "Pick CSV 2 (ID list)",
-                    icon=flet_core.icons.UPLOAD_FILE,
-                    on_click=lambda _: my_picker2.pick_files(allow_multiple=False)
-                ),
-                csv2_location,
-                ElevatedButton(
-                    "Process",
-                    on_click=process_search
+                    "Hash",
+                    on_click=process_hashing
                 ),
                 result,
                 Container(
